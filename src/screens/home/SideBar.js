7ed45df72121c78bc0c7ff92609f1fd2bf766eda
Sunny Cletus <scletus@gmail.com>
2020-05-27 20:28:51 +0100

    update made to sidebar

--- src/screens/home/SideBar.js
+++ src/screens/home/SideBar.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import styled from "styled-components";
 
 const Div = styled.div`
-background:red;
-margin:0;
-`
+  background: red;
+  margin: 0;
+`;
+const Search = styled(Form)`
+  margin: 20px;
+`;
 export const SideBar = () => (
-    <Div className='col-sm-3'>
-        <div style={{background:'green'}}>
-        Hello gus this is a sidebar
-        </div>
-    </Div>
-)
+  <Div className="col-sm-3">
+    <Search>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label srOnly>Search</Form.Label>
+        <Form.Control type="text" placeholder="search..." />
+      </Form.Group>
+    </Search>
+  </Div>
+);
